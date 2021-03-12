bb2c886bcf224dd36aaf53d0dffa176bbb7d900b
rlysell <rlysell@kth.se>
2021-03-12 14:21:58 +0100

    Added CanConstruct.py

---
+++ CanConstruct.py
@@ -0,0 +1,28 @@
+def Can_construct(TargetWord, wordbank, memo = {}):
+    if TargetWord in memo:
+        return memo[TargetWord]
+    if TargetWord == "":
+        return True
+    for word in wordbank:
+        if TargetWord.find(word) == 0:
+            suffix = TargetWord[len(word):]
+            if Can_construct(suffix, wordbank, memo):
+                memo[TargetWord] = True
+                return True
+    memo[TargetWord] = False
+    return False
+
+print(Can_construct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
+print(Can_construct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+
+print(Can_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", 
+["ee", 
+"eee", 
+"eeee", 
+"eeeee", 
+"eeeeee", 
+"eeeeeee", 
+"eeeeeeee"]))
+
+
+
