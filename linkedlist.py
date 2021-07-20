94ded845054098a6f7639197f196102cd7cb0b04
rlysell <rlysell@kth.se>
2021-07-20 17:01:34 +0200

    Added linkedlist.py

---
+++ linkedlist.py
@@ -0,0 +1,38 @@
+class Node():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+n5.next = n6
+
+node = n1
+print("Original linked list:")
+while(node != None):
+    print(node.value)
+    node = node.next
+
+def reverse_linked_list(head):
+    if head == None or head.next == None:
+        return head
+    new_head = reverse_linked_list(head.next)
+    head.next.next = head
+    head.next= None
+    return new_head
+
+
+node = reverse_linked_list(n1)
+print("\nReversed linked list:")
+while(node != None):
+    print(node.value)
+    node = node.next
